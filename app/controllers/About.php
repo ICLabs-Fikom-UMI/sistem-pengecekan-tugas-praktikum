377a6d7c5f83ebdd86dfdecc85f0d2f744bd3a64
AmiC <126696356+NurulAzmi0066@users.noreply.github.com>
2024-01-23 21:28:52 +0800

    Delete app directory

--- app/controllers/About.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class About {
-    public function page(){
-        echo 'About/page';    
-    }
-}
--- app/models/User_model.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class User_model {
-    private $name = 'Azmi';
-
-    public function getUser(){
-        return $this->name;
-    }
-}
